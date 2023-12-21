33c166e757e6ba95aefad8af160114a2d6555ce4
Gautam Adhikari <gautamadhikari831@gmail.com>
2023-12-22 00:12:52 +0530

    contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
         purpleColor: "#9771FF",
         irisBlueColor: "#01B5C5",
         headingColor: "#181A1E",
-        textColor: "#4545F",
+        textColor: "#4E545F",
     },
     boxShadow: {
       panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",

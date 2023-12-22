f73e4ef485f086a4ff480a99f4026b08b0168a9e
Gautam Adhikari <gautamadhikari831@gmail.com>
2023-12-22 11:54:12 +0530

    final

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -4,6 +4,7 @@ import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import patientAvatar from '../../assets/images/patient-avatar.png'
+import review from '../../assets/images/review.png'
 import {HiStar} from 'react-icons/hi'
 
 const Testimonial = () => {
@@ -50,7 +51,7 @@ const Testimonial = () => {
         <SwiperSlide>
         <div className="py-[30px] px-5 rounded-3">
         <div className="flex items-center gap-[13px]">
-        <img src="/src/assets/images/review.png" alt="" class="h-10"></img>
+        <img src={review} alt="" class="h-10"></img>
         <div><h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">Priya Sharma</h4>
         <div className="flex items-center gap-[2px]">
         <HiStar className=" text-yellowColor w-[18px] h-5" /> 

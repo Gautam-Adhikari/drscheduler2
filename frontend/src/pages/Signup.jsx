7e6a51dff319b7d51132a55b207de58e9a35e97c
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-05-07 12:17:35 +0530

    updates

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import signupImg from '../assets/images/signup.gif'
 import avatar from '../assets/images/doctor-img01.png'
-import { Link, useNavigate } from 'react-router-dom';
-import uploadImageToCloudinary from '../utils/uploadCloudinary';
-import { BASE_URL } from '../config';
-import {toast} from 'react-toastify'
+import { Link, useNavigate } from 'react-router-dom'; //navigate
+import uploadImageToCloudinary from '../utils/uploadCloudinary'; //image in cloud
+import { BASE_URL } from '../config'; //base url of backend api
+import {toast} from 'react-toastify'  //notifications
 import HashLoader from 'react-spinners/HashLoader'
 
 const Signup = () => {
